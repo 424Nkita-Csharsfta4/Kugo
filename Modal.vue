6bf9c4e8ff5237de3142f7b4a3af764b929a38d9
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2023-02-19 23:28:27 +0200

    Add files via upload

--- Modal.vue
+++ Modal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed z-10 inset-0 overflow-y-auto" v-if="isOpen" @click.self="">
+  <div class="fixed z-10 inset-0 overflow-y-auto">
     <div class="flex items-center justify-center min-h-screen">
       <div class="bg-white rounded-lg shadow-lg p-6">
        <slot/>
@@ -8,33 +8,4 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, watch } from 'vue'
 
-interface ModalProps {
-  isOpen: {
-    type: BooleanConstructor;
-    required: true;
-  };
-  onClose: {
-    type: FunctionConstructor;
-    required: true;
-  };
-}
-
-
-export default defineComponent({
-  name: 'Modal',
-  props: {
-    isOpen: {
-      type: Boolean,
-      required: true,
-    },
-    onClose: {
-      type: Function,
-      required: true,
-    },
-  },
- 
-})
-</script>
