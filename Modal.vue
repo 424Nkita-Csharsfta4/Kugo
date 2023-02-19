ff2bed0375cd3df7156b9d157df255a1aced9903
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2023-02-19 23:29:38 +0200

    Add files via upload

--- Modal.vue
+++ Modal.vue
@@ -2,7 +2,7 @@
   <div class="fixed z-10 inset-0 overflow-y-auto">
     <div class="flex items-center justify-center min-h-screen">
       <div class="bg-white rounded-lg shadow-lg p-6">
-       <slot/>
+       
       </div>
     </div>
   </div>
